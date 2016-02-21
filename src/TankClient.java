b1591ddf6cc218588d7a26b53d284d821beee8be
C.y <yangruihan@vip.qq.com>
2016-02-22 00:54:20 +0800

    v0.4 使坦克动起来

--- src/TankClient.java
+++ src/TankClient.java
@@ -7,31 +7,43 @@ import javax.swing.WindowConstants;
 
 /**
  * 坦克大战客户端
+ * 
  * @author Yrh
  *
  */
 public class TankClient extends JFrame {
-	
+
 	private static int WIDTH = 800; // 窗口宽度
 	private static int HEIGHT = 600; // 窗口高度
-	
+
+	private int x = 50; // 坦克 x 坐标
+	private int y = 50; // 坦克 y 坐标
+
+	/*
+	 * 绘制方法
+	 */
 	@Override
 	public void paint(Graphics g) {
 		// 保存当前颜色
 		Color c = g.getColor();
-		
+
 		// 设置颜色并绘制背景
 		g.setColor(Color.GREEN);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
+
 		// 设置颜色并画圆
 		g.setColor(Color.RED);
-		g.fillOval(50, 50, 30, 30);
-		
+		g.fillOval(x, y, 30, 30);
+
 		// 还原颜色
 		g.setColor(c);
+
+		y += 5;
 	}
-	
+
+	/*
+	 * 加载 Frame 方法
+	 */
 	public void lanchFrame() {
 		// 设置窗口大小
 		this.setSize(WIDTH, HEIGHT);
@@ -43,10 +55,33 @@ public class TankClient extends JFrame {
 		this.setResizable(false);
 		// 设置窗口是否可见
 		this.setVisible(true);
+
+		new Thread(new PaintThread()).start();
 	}
-	
+
 	public static void main(String[] args) {
 		TankClient tankClient = new TankClient();
 		tankClient.lanchFrame();
+	}
+
+	/**
+	 * 绘制线程类
+	 * @author Yrh
+	 *
+	 */
+	private class PaintThread implements Runnable {
+
+		@Override
+		public void run() {
+			while (true) {
+				// 重绘整个窗口
+				repaint();
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
