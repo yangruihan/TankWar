9167fa6439a77fee91d75d9f9807a8d5918d6b35
C.y <yangruihan@vip.qq.com>
2016-02-22 00:40:00 +0800

    v0.3 draw a circle as tank and background

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,4 +1,7 @@
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
@@ -9,9 +12,29 @@ import javax.swing.WindowConstants;
  */
 public class TankClient extends JFrame {
 	
+	private static int WIDTH = 800; // 窗口宽度
+	private static int HEIGHT = 600; // 窗口高度
+	
+	@Override
+	public void paint(Graphics g) {
+		// 保存当前颜色
+		Color c = g.getColor();
+		
+		// 设置颜色并绘制背景
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		// 设置颜色并画圆
+		g.setColor(Color.RED);
+		g.fillOval(50, 50, 30, 30);
+		
+		// 还原颜色
+		g.setColor(c);
+	}
+	
 	public void lanchFrame() {
 		// 设置窗口大小
-		this.setSize(800, 600);
+		this.setSize(WIDTH, HEIGHT);
 		// 设置标题文字
 		this.setTitle("TankWar");
 		// 设置默认关闭操作
